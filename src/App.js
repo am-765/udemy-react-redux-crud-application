1f4ac52292e17eb5b54943b1044106e570b06ae3
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2020-12-14 15:53:55 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { div } from 'prelude-ls';
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => console.log('I am clicked!')} />
+      </div>
+    )
   }
 }
 
